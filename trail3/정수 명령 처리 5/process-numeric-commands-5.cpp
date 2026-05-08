9f61806c20374e9e349c7f95225b160a19fbf864
pnpn4 <166359662+pnpn4@users.noreply.github.com>
2026-05-08 18:37:49 +0900

    [Passed] 정수 명령 처리 5 (13ms, 1MB)

---
+++ trail3/정수 명령 처리 5/process-numeric-commands-5.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N;
+string command[10000];
+int num[10000];
+
+int main() {
+    cin >> N;
+    vector<int> v;
+    for (int i = 0; i < N; i++) {
+        cin >> command[i];
+        if (command[i] == "push_back" || command[i] == "get") {
+            cin >> num[i];
+        }
+        if(command[i]=="push_back")
+            v.push_back(num[i]);
+        if(command[i]=="pop_back")
+            v.pop_back();
+        if(command[i]=="size")
+            cout << v.size() << endl;
+        if(command[i]=="get")
+            cout << v[num[i]-1] << endl;
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
